4196c3608adbc3223ad6eb346a04f5e8549ed378
nicolasult <nicolas.ultrillo@gmail.com>
2025-05-25 13:13:23 +0200

    Add basic game class

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.displayer.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Pong")
+        self.clock = pygame.time.Clock()
+        self.running = True
+    
+    def run(self):
+        while self.running == True:
+            dt = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # update
+
+            # draw
+            pygame.display.update()
+        pygame.quit()
