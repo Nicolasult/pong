4e48dd4993c96009d4a9d202cc3c380437f4bcc7
nicolasult <nicolas.ultrillo@gmail.com>
2025-05-25 13:27:27 +0200

    handle player movement

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from settings import *
+from sprites import *
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.display_surface = pygame.displayer.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Pong")
         self.clock = pygame.time.Clock()
         self.running = True
+
+        # sprites
+        self.all_sprites = pygame.sprite.Group()
+        self.paddle_sprites = pygame.sprite.Group()
+        self.player = Player((self.all_sprites, self.paddle_sprites))
     
     def run(self):
         while self.running == True:
@@ -16,7 +22,13 @@ class Game:
                     self.running = False
 
             # update
-
+            self.all_sprites.update(dt)
             # draw
+            self.display_surface.fill(COLORS["bg"])
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
         pygame.quit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
